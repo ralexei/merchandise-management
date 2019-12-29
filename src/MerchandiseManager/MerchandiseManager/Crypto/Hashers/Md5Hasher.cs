5e367df2aca3f909409daf0d92ea1df37e9e1729
Alexei <alexei.r@primefin.tech>
2019-12-29 04:27:31 +0200

    Added Utility class Lib
    -Added Md5 hashing helper
    -Added extension method for DateTime - ToUnix...

---
+++ src/MerchandiseManager/MerchandiseManager/Crypto/Hashers/Md5Hasher.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MerchandiseManager.Utility.Crypto.Hashers
+{
+	public static class Md5Hasher
+	{
+		/// <summary>
+		/// Generates a hash of a given string
+		/// </summary>
+		/// <param name="key">string to hash</param>
+		/// <returns>MD5 hash of the given string</returns>
+		public static string Generate(string key)
+		{
+			using (var md5Encryptor = MD5.Create())
+			{
+				return BitConverter.ToString(md5Encryptor.ComputeHash(Encoding.UTF8.GetBytes(key))).Replace("-", "");
+			}
+		}
+
+		public static byte[] GenerateBytes(string key)
+		{
+			using (var md5Encryptor = MD5.Create())
+			{
+				return md5Encryptor.ComputeHash(Encoding.UTF8.GetBytes(key));
+			}
+		}
+
+		/// <summary>
+		/// Generate an input independent "random" hash, based on ticks
+		/// </summary>
+		/// <returns>random MD5 hash</returns>
+		public static string Generate()
+		{
+			return Generate(DateTime.UtcNow.Ticks.ToString());
+		}
+	}
+}
---
+++ src/MerchandiseManager/MerchandiseManager/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MerchandiseManager.Utility.Extensions
+{
+	public static class DateTimeExtensions
+	{
+		public static long ToUnixTimestamp(this DateTime dt)
+		{
+			return (long)dt.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+		}
+	}
+}
