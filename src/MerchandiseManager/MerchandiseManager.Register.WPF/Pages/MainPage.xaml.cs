3c70c56666138a392ac5d28000bbcaead15393e3
Alexei <alexei.ru1997@gmail.com>
2021-01-13 02:32:53 +0200

    Changes for the RegisterApp main page

--- src/MerchandiseManager/MerchandiseManager.Register.WPF/Pages/MainPage.xaml.cs
+++ src/MerchandiseManager/MerchandiseManager.Register.WPF/Pages/MainPage.xaml.cs
@@ -24,11 +24,50 @@ namespace MerchandiseManager.Register.WPF.Pages
 			InitializeComponent();
 		}
 
+		private void Window_KeyDown(object sender, KeyEventArgs e)
+		{
+			// check timing (keystrokes within 100 ms)
+			TimeSpan elapsed = (DateTime.Now - _lastKeystroke);
+			if (elapsed.TotalMilliseconds > 100)
+				_barcode.Clear();
+
+			var keyChar = KeyInterop.VirtualKeyFromKey(e.Key);
+
+			// process barcode
+			if (keyChar == 13 && _barcode.Count > 0)
+			{
+				string msg = string.Join("", _barcode);
+				//MessageBox.Show(msg); // Do something
+				_barcode.Clear();
+			}
+			else
+			{
+				// record keystroke & timestamp
+				_barcode.Add((char)keyChar);
+				_lastKeystroke = DateTime.Now;
+			}
+		}
+
 		private void Button_Click(object sender, RoutedEventArgs e)
 		{
 			var loginForm = new LoginWindow();
 
 			loginForm.Show();
+		}
+
+		private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+
+		}
+
+		DateTime _lastKeystroke = new DateTime(0);
+		List<char> _barcode = new List<char>(20);
+
+		private void Page_Loaded(object sender, RoutedEventArgs e)
+		{
+			var window = Window.GetWindow(this);
+
+			window.KeyDown += Window_KeyDown;
 		}
 	}
 }
